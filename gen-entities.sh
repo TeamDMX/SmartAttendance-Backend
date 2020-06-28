5262dc23f8d8f100989ed786d372671167e9d5c1
ipmanlk <io@navinda.xyz>
2020-06-28 18:46:01 +0530

    change: updated configs for new schema name

--- gen-entities.sh
+++ gen-entities.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-npx typeorm-model-generator -h localhost -d smartattendacne -u root -x "" -e mysql -o /tmp/
+typeorm-model-generator -h localhost -d smart_attendance -u root -x "" -e mysql -o /tmp/
 mv /tmp/entities/* ./src/entity
