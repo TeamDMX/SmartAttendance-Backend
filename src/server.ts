2947039e33b93076e74107f76ece3c939c644a0a
ipmanlk <io@navinda.xyz>
2020-07-01 11:06:17 +0530

    add: basic express server with sessions

---
+++ src/server.ts
@@ -0,0 +1,105 @@
+/* 
+=====================================================================================
+DOTENV: Load settings from .env to process.env (see sample.env)
+=====================================================================================
+*/
+
+require("dotenv").config();
+
+/* 
+=====================================================================================
+Libraries
+=====================================================================================
+*/
+
+// Libraries: 3rd Party
+import { createConnection } from "typeorm";
+import * as express from "express";
+import * as session from "express-session";
+
+/* 
+=====================================================================================
+Controllers
+=====================================================================================
+*/
+
+
+/* 
+=====================================================================================
+Utilities
+=====================================================================================
+*/
+
+// import { RegexPatternUtil } from "./util/RegexPatternUtil";
+
+/* 
+=====================================================================================
+TypeORM
+=====================================================================================
+*/
+
+// TypeORM: Create connection to the detabase
+createConnection().then(() => {
+   console.log("SUCESS: Database connected.");
+}).catch(error => {
+   console.log("ERROR: Database  connection failed.");
+   throw Error(error);
+});
+
+/* 
+=====================================================================================
+Express.js
+=====================================================================================
+*/
+
+// method used for checking permissions
+// const isAuthorized = AuthController.isAuthorized;
+
+// Express.js: Initialize
+const app = express();
+
+// Express.js: Parse json request bodies
+app.use(express.json({
+   limit: "8000kb"
+}));
+
+// Express.js: Sessions for login
+app.use(session({
+   secret: process.env.SESSION_SECRET,
+   saveUninitialized: false,
+   resave: false
+}));
+
+// enable CORS on development enviroment
+if (process.env.PRODUCTION == "false") {
+   app.use((req, res, next) => {
+      res.header("Access-Control-Allow-Origin", "*");
+      res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+      next();
+   });
+}
+
+// Express.js: Folder with static HTML files to server the user
+app.use("/", express.static(`${__dirname}/../../public`));
+
+// skip check for development enviroments
+// app.use((req, res, next) => {
+//    if (process.env.PRODUCTION == "false") {
+//       req.session.data = {};
+//       req.session.data.username = "admin";
+//       req.session.data.logged = true;
+//       req.session.data.userRoles = [{ id: 1 }, { id: 2 }];
+//       req.session.data.userId = { id: 1 };
+//    }
+//    next();
+// });
+
+/* 
+=====================================================================================
+Routes
+=====================================================================================
+*/
+
+
+// Express.js: Start the server
+app.listen(process.env.PORT, () => console.log(`Server is running on ${process.env.PORT}!`));
