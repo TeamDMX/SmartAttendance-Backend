8facc231d80505bb10f3582dc85713d46d372636
ipmanlk <io@navinda.xyz>
2021-01-24 20:42:49 +0530

    fix: user update crash

--- src/controller/UserController.ts
+++ src/controller/UserController.ts
@@ -10,300 +10,324 @@ import { ValidationUtil } from "../util/ValidationUtil";
 import * as crypto from "crypto";
 
 export class UserController {
+	static async getOne(studentId: number) {
+		// check if valid data is given
+		await ValidationUtil.validate("USER", { studentId });
 
-    static async getOne(studentId: number) {
-        // check if valid data is given
-        await ValidationUtil.validate("USER", { studentId });
+		// search for an entry with given id
+		const entry = await UserDao.getOne(studentId);
 
-        // search for an entry with given id
-        const entry = await UserDao.getOne(studentId)
+		// check if entry exists
+		if (entry !== undefined) {
+			return {
+				status: true,
+				data: entry,
+			};
+		} else {
+			throw {
+				status: false,
+				type: "input",
+				msg: "Unable to find an entry with that id.",
+			};
+		}
+	}
 
-        // check if entry exists
-        if (entry !== undefined) {
-            return {
-                status: true,
-                data: entry
-            };
-        } else {
-            throw {
-                status: false,
-                type: "input",
-                msg: "Unable to find an entry with that id."
-            };
-        }
-    }
+	static async getMany(keyword: string, skip: number) {
+		const entries = await UserDao.search(keyword, skip).catch((e) => {
+			console.log(e.code, e);
+			throw {
+				status: false,
+				type: "server",
+				msg: "Server Error!. Please check logs.",
+			};
+		});
 
-    static async getMany(keyword: string, skip: number) {
-        const entries = await UserDao.search(keyword, skip).catch(e => {
-            console.log(e.code, e);
-            throw {
-                status: false,
-                type: "server",
-                msg: "Server Error!. Please check logs."
-            }
-        });
+		return {
+			status: true,
+			data: entries,
+		};
+	}
 
-        return {
-            status: true,
-            data: entries
-        };
-    }
+	static async save(data) {
+		// check if valid data is given
+		await ValidationUtil.validate("USER", data);
 
-    static async save(data) {
-        // check if valid data is given
-        await ValidationUtil.validate("USER", data);
+		// check if both lecCode and Student Reg number are provided
+		if (data.lecCode != "null" && data.stuRegNumber != "null") {
+			throw {
+				status: false,
+				type: "input",
+				msg: "Invalid user type info!.",
+			};
+		}
 
-        // check if both lecCode and Student Reg number are provided
-        if (data.lecCode != "null" && data.stuRegNumber != "null") {
-            throw {
-                status: false,
-                type: "input",
-                msg: "Invalid user type info!."
-            }
-        }
+		const entry = data as User;
 
-        const entry = data as User;
+		// find student / lecturer
+		let lecturer = undefined,
+			student = undefined;
+		try {
+			if (data.lecCode != "null") {
+				lecturer = await getRepository(Lecturer).findOne({
+					where: { code: data.lecCode },
+				});
+				if (!lecturer) {
+					throw {
+						status: false,
+						type: "input",
+						msg: "Lecturer with that code doesn't exist.",
+					};
+				}
+				entry.lecturerId = lecturer.id;
+			} else if (data.stuRegNumber != "null") {
+				student = await getRepository(Student).findOne({
+					where: { regNumber: data.stuRegNumber },
+				});
+				if (!student) {
+					throw {
+						status: false,
+						type: "input",
+						msg: "Student with that registration number doesn't exist.",
+					};
+				}
 
-        // find student / lecturer
-        let lecturer = undefined, student = undefined;
-        try {
-            if (data.lecCode != "null") {
-                lecturer = await getRepository(Lecturer).findOne({ where: { code: data.lecCode } });
-                if (!lecturer) {
-                    throw {
-                        status: false,
-                        type: "input",
-                        msg: "Lecturer with that code doesn't exist."
-                    }
-                }
-                entry.lecturerId = lecturer.id;
+				entry.studentId = student.id;
+				entry.isActive = false;
+			}
+		} catch (e) {
+			console.log(e);
+			if (e.status == true) throw e;
+			throw {
+				status: false,
+				type: "server",
+				msg: "Server Error!. Please check logs.",
+			};
+		}
 
-            } else if (data.stuRegNumber != "null") {
-                student = await getRepository(Student).findOne({ where: { regNumber: data.stuRegNumber } });
-                if (!student) {
-                    throw {
-                        status: false,
-                        type: "input",
-                        msg: "Student with that registration number doesn't exist."
-                    }
-                }
+		// hash passwrod
+		entry.password = crypto
+			.createHash("sha512")
+			.update(entry.password)
+			.digest("hex");
 
-                entry.studentId = student.id;
-                entry.isActive = false;
-            }
+		const newUser = await getRepository(User)
+			.save(entry)
+			.catch((e) => {
+				console.log(e.code, e);
 
-        } catch (e) {
-            console.log(e);
-            if (e.status == true) throw e;
-            throw {
-                status: false,
-                type: "server",
-                msg: "Server Error!. Please check logs."
-            }
-        }
+				if (e.code == "ER_DUP_ENTRY") {
+					throw {
+						status: false,
+						type: "input",
+						msg: "Entry already exists!.",
+					};
+				}
+				throw {
+					status: false,
+					type: "server",
+					msg: "Server Error!. Please check logs.",
+				};
+			});
 
-        // hash passwrod
-        entry.password = crypto.createHash("sha512").update(entry.password).digest("hex");
+		// set roles for new user
+		const roleIds = data.roleIds.split(",");
 
-        const newUser = await getRepository(User).save(entry).catch(e => {
-            console.log(e.code, e);
+		const userRoles = roleIds.map((rid) => {
+			return { userId: newUser.id, roleId: rid };
+		});
 
-            if (e.code == "ER_DUP_ENTRY") {
-                throw {
-                    status: false,
-                    type: "input",
-                    msg: "Entry already exists!."
-                }
-            }
-            throw {
-                status: false,
-                type: "server",
-                msg: "Server Error!. Please check logs."
-            }
-        });
+		await getRepository(UserRole)
+			.save(userRoles)
+			.catch((e) => {
+				console.log(e.code, e);
+				if (e.code == "ER_DUP_ENTRY") {
+					throw {
+						status: false,
+						type: "input",
+						msg: "User roles already exists!.",
+					};
+				}
+				throw {
+					status: false,
+					type: "server",
+					msg: "Server Error!. Please check logs. (Role adding)",
+				};
+			});
 
-        // set roles for new user
-        const roleIds = data.roleIds.split(",");
+		return {
+			status: true,
+			msg: "That entry has been added!",
+		};
+	}
 
-        const userRoles = roleIds.map(rid => {
-            return { userId: newUser.id, roleId: rid }
-        });
+	static async update(userId: number, data) {
+		// create entry object
+		data.id = userId;
 
-        await getRepository(UserRole).save(userRoles).catch(e => {
-            console.log(e.code, e);
-            if (e.code == "ER_DUP_ENTRY") {
-                throw {
-                    status: false,
-                    type: "input",
-                    msg: "User roles already exists!."
-                }
-            }
-            throw {
-                status: false,
-                type: "server",
-                msg: "Server Error!. Please check logs. (Role adding)"
-            }
-        });
+		// check if valid data is given
+		await ValidationUtil.validate("USER", data);
 
-        return {
-            status: true,
-            msg: "That entry has been added!"
-        };
-    }
+		// check if both lecCode and Student Reg number are provided
+		if (data.lecCode != "null" && data.stuRegNumber != "null") {
+			throw {
+				status: false,
+				type: "input",
+				msg: "Invalid user type info!.",
+			};
+		}
 
-    static async update(userId: number, data) {
-        // create entry object
-        data.id = userId;
+		const editedEntry = data as User;
 
-        // check if valid data is given
-        await ValidationUtil.validate("USER", data);
+		// check if an entry is present with the given id
+		const selectedEntry = await getRepository(User)
+			.findOne(editedEntry.id)
+			.catch((e) => {
+				console.log(e.code, e);
+				throw {
+					status: false,
+					type: "server",
+					msg: "Server Error!. Please check logs.",
+				};
+			});
 
+		if (!selectedEntry) {
+			throw {
+				status: false,
+				type: "input",
+				msg: "That entry doesn't exist in our database!.",
+			};
+		}
 
-        // check if both lecCode and Student Reg number are provided
-        if (data.lecCode != "null" && data.stuRegNumber != "null") {
-            throw {
-                status: false,
-                type: "input",
-                msg: "Invalid user type info!."
-            }
-        }
+		// find student / lecturer
+		let lecturer = undefined,
+			student = undefined;
+		try {
+			if (data.lecCode != "null") {
+				lecturer = await getRepository(Lecturer).findOne({
+					where: { code: data.lecCode },
+				});
+				if (!lecturer) {
+					throw {
+						status: false,
+						type: "input",
+						msg: "Lecturer with that code doesn't exist.",
+					};
+				}
+				editedEntry.lecturerId = lecturer.id;
+			} else if (data.stuRegNumber != "null") {
+				student = await getRepository(Student).findOne({
+					where: { regNumber: data.stuRegNumber },
+				});
+				if (!student) {
+					throw {
+						status: false,
+						type: "input",
+						msg: "Student with that registration number doesn't exist.",
+					};
+				}
 
-        const editedEntry = data as User;
+				editedEntry.studentId = student.id;
+			}
+		} catch (e) {
+			console.log(e);
+			if (e.status) throw e;
+			throw {
+				status: false,
+				type: "server",
+				msg: "Server Error!. Please check logs.",
+			};
+		}
 
-        // check if an entry is present with the given id
-        const selectedEntry = await getRepository(User).findOne(editedEntry.id).catch(e => {
-            console.log(e.code, e);
-            throw {
-                status: false,
-                type: "server",
-                msg: "Server Error!. Please check logs."
-            }
-        });
+		// hash passwrod
+		editedEntry.password = crypto
+			.createHash("sha512")
+			.update(editedEntry.password)
+			.digest("hex");
 
-        if (!selectedEntry) {
-            throw {
-                status: false,
-                type: "input",
-                msg: "That entry doesn't exist in our database!."
-            }
-        }
+		// update entry
+		await getRepository(User)
+			.save(editedEntry)
+			.catch((e) => {
+				console.log(e.code, e);
+				throw {
+					status: false,
+					type: "server",
+					msg: "Server Error!. Please check logs.",
+				};
+			});
 
+		// delete exisitng user roles
+		await getRepository(UserRole).delete({ userId: editedEntry.id });
 
-        // find student / lecturer
-        let lecturer = undefined, student = undefined;
-        try {
-            if (data.lecCode != null) {
-                lecturer = await getRepository(Lecturer).findOne({ where: { code: data.lecCode } });
-                if (!lecturer) {
-                    throw {
-                        status: false,
-                        type: "input",
-                        msg: "Lecturer with that code doesn't exist."
-                    }
-                }
-                editedEntry.lecturerId = lecturer.id;
+		// update roles
+		const roleIds = data.roleIds.split(",");
+		const userRoles = roleIds.map((rid) => {
+			return { userId: editedEntry.id, roleId: rid };
+		});
 
-            } else if (data.stuRegNumber != null) {
-                student = await getRepository(Student).findOne({ where: { regNumber: data.stuRegNumber } });
-                if (!student) {
-                    throw {
-                        status: false,
-                        type: "input",
-                        msg: "Student with that registration number doesn't exist."
-                    }
-                }
+		await getRepository(UserRole)
+			.save(userRoles)
+			.catch((e) => {
+				console.log(e.code, e);
+				if (e.code == "ER_DUP_ENTRY") {
+					throw {
+						status: false,
+						type: "input",
+						msg: "User roles already exists!.",
+					};
+				}
+				throw {
+					status: false,
+					type: "server",
+					msg: "Server Error!. Please check logs. (Role adding)",
+				};
+			});
 
-                editedEntry.studentId = student.id;
-            }
+		return {
+			status: true,
+			msg: "That entry has been updated!",
+		};
+	}
 
-        } catch (e) {
-            if (e.status) throw e;
-            throw {
-                status: false,
-                type: "server",
-                msg: "Server Error!. Please check logs."
-            }
-        }
+	static async delete(userId: number) {
+		// check if valid data is given
+		await ValidationUtil.validate("USER", userId);
 
-        // hash passwrod
-        editedEntry.password = crypto.createHash("sha512").update(editedEntry.password).digest("hex");
+		// find the entry with the given id
+		const entry = await getRepository(User)
+			.findOne({ id: userId })
+			.catch((e) => {
+				console.log(e.code, e);
+				throw {
+					status: false,
+					type: "server",
+					msg: "Server Error!. Please check logs.",
+				};
+			});
 
-        // update entry
-        await getRepository(User).save(editedEntry).catch(e => {
-            console.log(e.code, e);
-            throw {
-                status: false,
-                type: "server",
-                msg: "Server Error!. Please check logs."
-            }
-        });
+		if (!entry) {
+			throw {
+				status: false,
+				type: "input",
+				msg: "That entry doesn't exist in our database!.",
+			};
+		}
 
-        // delete exisitng user roles
-        await getRepository(UserRole).delete({ userId: editedEntry.id });
+		// delete the entry
+		await getRepository(User)
+			.delete(entry)
+			.catch((e) => {
+				console.log(e.code, e);
+				throw {
+					status: false,
+					type: "server",
+					msg: "Server Error!. Please check logs.",
+				};
+			});
 
-        // update roles
-        const roleIds = data.roleIds.split(",");
-        const userRoles = roleIds.map(rid => {
-            return { userId: editedEntry.id, roleId: rid }
-        });
-
-        await getRepository(UserRole).save(userRoles).catch(e => {
-            console.log(e.code, e);
-            if (e.code == "ER_DUP_ENTRY") {
-                throw {
-                    status: false,
-                    type: "input",
-                    msg: "User roles already exists!."
-                }
-            }
-            throw {
-                status: false,
-                type: "server",
-                msg: "Server Error!. Please check logs. (Role adding)"
-            }
-        });
-
-        return {
-            status: true,
-            msg: "That entry has been updated!"
-        };
-    }
-
-    static async delete(userId: number) {
-        // check if valid data is given
-        await ValidationUtil.validate("USER", userId);
-
-        // find the entry with the given id
-        const entry = await getRepository(User).findOne({ id: userId }).catch(e => {
-            console.log(e.code, e);
-            throw {
-                status: false,
-                type: "server",
-                msg: "Server Error!. Please check logs."
-            }
-        });
-
-        if (!entry) {
-            throw {
-                status: false,
-                type: "input",
-                msg: "That entry doesn't exist in our database!."
-            }
-        }
-
-        // delete the entry
-        await getRepository(User).delete(entry).catch(e => {
-            console.log(e.code, e);
-            throw {
-                status: false,
-                type: "server",
-                msg: "Server Error!. Please check logs."
-            }
-        });
-
-        return {
-            status: true,
-            msg: "That entry has been deleted!"
-        };
-    }
+		return {
+			status: true,
+			msg: "That entry has been deleted!",
+		};
+	}
 }
